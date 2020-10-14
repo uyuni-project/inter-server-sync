6d4e9da898a490ceeac77c0b195dc1ab0661afb0
Silvio Moioli <moio@suse.com>
2020-10-14 16:44:55 +0200

    Refactoring: move schema dumping away

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
-	"strings"
 
 	_ "github.com/lib/pq"
 	"github.com/moio/mgr-dump/schemareader"
@@ -15,7 +13,6 @@ const connectionString = "user='spacewalk' password='spacewalk' dbname='susemana
 
 // psql --host=localhost --port=5432 --username=spacewalk susemanager
 
-// go run . | dot -Tx11
 func main() {
 	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
@@ -24,53 +21,5 @@ func main() {
 	defer db.Close()
 	tables := schemareader.ReadTables(db)
 
-	fmt.Printf("graph schema {\n")
-	fmt.Printf("  layout=fdp;")
-	fmt.Printf("  K=0.15;")
-	fmt.Printf("  maxiter=1000;")
-	fmt.Printf("  start=0;")
-
-	for _, table := range tables {
-		fmt.Printf("\"%s\" [shape=box];\n", table.Name)
-
-		for _, column := range table.Columns {
-			_, primary := table.PKColumns[column]
-			color := "transparent"
-			if primary {
-				color = "gainsboro"
-			}
-			fmt.Printf("\"%s-%s\" [label=\"\" xlabel=\"%s\" style=filled fillcolor=\"%s\"];\n", table.Name, column, column, color)
-			fmt.Printf("\"%s\" -- \"%s-%s\";\n", table.Name, table.Name, column)
-		}
-
-		if len(table.PKSequence) > 0 {
-			fmt.Printf("\"%s-id-%s\" [label=\"%s\" shape=note];\n", table.Name, table.PKSequence, table.PKSequence)
-			fmt.Printf("\"%s-id\" -- \"%s-id-%s\" [style=dashed];\n", table.Name, table.Name, table.PKSequence)
-		}
-
-		for _, index := range table.UniqueIndexes {
-			label := "unique"
-			if len(table.MainUniqueIndexName) > 0 {
-				if strings.Compare(index.Name, table.MainUniqueIndexName) == 0 {
-					label = "unique main"
-				}
-			}
-			fmt.Printf("\"%s\" [label=\"%s\" shape=tab];\n", index.Name, label)
-
-			for _, indexColumn := range index.Columns {
-				fmt.Printf("\"%s-%s\" -- \"%s\" [style=dashed];\n", table.Name, indexColumn, index.Name)
-			}
-		}
-
-		for i, reference := range table.References {
-			fmt.Printf("\"%s-%s-%d\" [label=\"\" shape=diamond];\n", table.Name, reference.TableName, i)
-
-			for column, foreignColumn := range reference.ColumnMapping {
-				fmt.Printf("\"%s-%s-%d\" -- \"%s-%s\";\n", table.Name, reference.TableName, i, table.Name, column)
-				fmt.Printf("\"%s-%s-%d\" -- \"%s-%s\";\n", table.Name, reference.TableName, i, reference.TableName, foreignColumn)
-			}
-		}
-	}
-
-	fmt.Printf("}")
+	schemareader.DumpToGraphviz(tables)
 }
---
+++ schemareader/dot.go
@@ -0,0 +1,60 @@
+package schemareader
+
+import (
+	"fmt"
+	"strings"
+)
+
+// DumpToGraphviz outputs a dot representation of a schema. Use:
+// go run . | dot -Tx11
+func DumpToGraphviz(tables []Table) {
+	fmt.Printf("graph schema {\n")
+	fmt.Printf("  layout=fdp;\n")
+	fmt.Printf("  K=0.15;\n")
+	fmt.Printf("  maxiter=1000;\n")
+	fmt.Printf("  start=0;\n\n")
+
+	for _, table := range tables {
+		fmt.Printf("\"%s\" [shape=box];\n", table.Name)
+
+		for _, column := range table.Columns {
+			_, primary := table.PKColumns[column]
+			color := "transparent"
+			if primary {
+				color = "gainsboro"
+			}
+			fmt.Printf("\"%s-%s\" [label=\"\" xlabel=\"%s\" style=filled fillcolor=\"%s\"];\n", table.Name, column, column, color)
+			fmt.Printf("\"%s\" -- \"%s-%s\";\n", table.Name, table.Name, column)
+		}
+
+		if len(table.PKSequence) > 0 {
+			fmt.Printf("\"%s-id-%s\" [label=\"%s\" shape=note];\n", table.Name, table.PKSequence, table.PKSequence)
+			fmt.Printf("\"%s-id\" -- \"%s-id-%s\" [style=dashed];\n", table.Name, table.Name, table.PKSequence)
+		}
+
+		for _, index := range table.UniqueIndexes {
+			label := "unique"
+			if len(table.MainUniqueIndexName) > 0 {
+				if strings.Compare(index.Name, table.MainUniqueIndexName) == 0 {
+					label = "unique main"
+				}
+			}
+			fmt.Printf("\"%s\" [label=\"%s\" shape=tab];\n", index.Name, label)
+
+			for _, indexColumn := range index.Columns {
+				fmt.Printf("\"%s-%s\" -- \"%s\" [style=dashed];\n", table.Name, indexColumn, index.Name)
+			}
+		}
+
+		for i, reference := range table.References {
+			fmt.Printf("\"%s-%s-%d\" [label=\"\" shape=diamond];\n", table.Name, reference.TableName, i)
+
+			for column, foreignColumn := range reference.ColumnMapping {
+				fmt.Printf("\"%s-%s-%d\" -- \"%s-%s\";\n", table.Name, reference.TableName, i, table.Name, column)
+				fmt.Printf("\"%s-%s-%d\" -- \"%s-%s\";\n", table.Name, reference.TableName, i, reference.TableName, foreignColumn)
+			}
+		}
+	}
+
+	fmt.Printf("}")
+}

22c22943e4b3cad45dd4c6eb7eb16fc2ed8a8c1c
Ricardo Mateus <rmateus@suse.com>
2020-10-19 16:21:40 +0100

    add missing pk sequences

--- schemareader/tableFilters.go
+++ schemareader/tableFilters.go
@@ -9,6 +9,12 @@ func applyTableFilters(table Table) Table {
 			table.PKSequence = "rhn_package_arch_id_seq"
 		case "rhnchannelarch":
 			table.PKSequence = "rhn_channel_arch_id_seq"
+		case "rhnpackagename":
+			// constraint: rhn_pn_id_pk
+			table.PKSequence = "rhn_checksum_id_seq"
+		case "rhnpackageevr":
+			// constraint: rhn_pe_id_pk
+			table.PKSequence = "rhn_pkg_evr_seq"
 		}
 	}
 	return table

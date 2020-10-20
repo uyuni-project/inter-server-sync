5eb67d301d0bf14984adedf6d3751919fb3ed5ba
Ricardo Mateus <rmateus@suse.com>
2020-10-20 14:02:59 +0100

    add virtual unique constraint for rhnpackage
    Signed-off-by: Ricardo Mateus <rmateus@suse.com>

--- schemareader/tableFilters.go
+++ schemareader/tableFilters.go
@@ -1,21 +1,26 @@
 package schemareader
 
 func applyTableFilters(table Table) Table {
-	if len(table.PKSequence) == 0 {
-		switch table.Name {
-		case "rhnchecksumtype":
-			table.PKSequence = "rhn_checksum_id_seq"
-		case "rhnpackagearch":
-			table.PKSequence = "rhn_package_arch_id_seq"
-		case "rhnchannelarch":
-			table.PKSequence = "rhn_channel_arch_id_seq"
-		case "rhnpackagename":
-			// constraint: rhn_pn_id_pk
-			table.PKSequence = "rhn_checksum_id_seq"
-		case "rhnpackageevr":
-			// constraint: rhn_pe_id_pk
-			table.PKSequence = "rhn_pkg_evr_seq"
-		}
+	switch table.Name {
+	case "rhnchecksumtype":
+		table.PKSequence = "rhn_checksum_id_seq"
+	case "rhnpackagearch":
+		table.PKSequence = "rhn_package_arch_id_seq"
+	case "rhnchannelarch":
+		table.PKSequence = "rhn_channel_arch_id_seq"
+	case "rhnpackagename":
+		// constraint: rhn_pn_id_pk
+		table.PKSequence = "rhn_checksum_id_seq"
+	case "rhnpackageevr":
+		// constraint: rhn_pe_id_pk
+		table.PKSequence = "rhn_pkg_evr_seq"
+	case "rhnpackage":
+		// We need to add a virtual unique constraint
+		table.PKSequence = "rhn_pkg_evr_seq"
+		virtualIndexName := "virtual_main_unique_index"
+		virtualIndexColumns := []string{"name_id", "evr_id", "package_arch_id", "checksum_id", "org_id"}
+		table.UniqueIndexes[virtualIndexName] = UniqueIndex{Name: virtualIndexName, Columns: virtualIndexColumns}
+		table.MainUniqueIndexName = virtualIndexName
 	}
 	return table
 }

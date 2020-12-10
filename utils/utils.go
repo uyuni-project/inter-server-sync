bb009b3c715be89e0704501043415e7c329ee682
Abid Mehmood <amehmood@suse.de>
2020-12-10 08:22:48 +0100

    Utility file with helper functions

---
+++ utils/utils.go
@@ -0,0 +1,22 @@
+package utils
+
+import "reflect"
+
+//ReverseArray reverses the array
+func ReverseArray(slice interface{}) {
+	size := reflect.ValueOf(slice).Len()
+	swap := reflect.Swapper(slice)
+	for i, j := 0, size-1; i < j; i, j = i+1, j-1 {
+		swap(i, j)
+	}
+}
+
+// Contains is a helper method to check if a string element exist in the string slice
+func Contains(slice []string, elementToFind string) bool {
+	for _, element := range slice {
+		if elementToFind == element {
+			return true
+		}
+	}
+	return false
+}

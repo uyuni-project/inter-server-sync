3e022af6b95e81910edaa8bc188747850fb5df56
Ricardo Mateus <rmateus@suse.com>
2021-01-15 22:20:19 +0000

    correct unique index update for rhnpackegeerv
    Signed-off-by: Ricardo Mateus <rmateus@suse.com>

--- schemareader/types.go
+++ schemareader/types.go
@@ -2,13 +2,14 @@ package schemareader
 
 // Table represents a DB table to dump
 type Table struct {
-	Name          string
-	Export        bool
-	Columns       []string
-	ColumnIndexes map[string]int
-	PKColumns     map[string]bool
-	PKSequence    string
-	UniqueIndexes map[string]UniqueIndex
+	Name            string
+	Export          bool
+	Columns         []string
+	UnexportColumns map[string]bool
+	ColumnIndexes   map[string]int
+	PKColumns       map[string]bool
+	PKSequence      string
+	UniqueIndexes   map[string]UniqueIndex
 	// a unique index is main when it is the preferred "natural" key
 	MainUniqueIndexName string
 	References          []Reference

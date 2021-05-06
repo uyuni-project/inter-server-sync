a2830d7fa0ac4ed7c1ec77e2f7b673e92a8344f4
Ricardo Mateus <rmateus@suse.com>
2021-05-06 11:22:25 +0100

    code review and refactoring
    Signed-off-by: Ricardo Mateus <rmateus@suse.com>

--- entityDumper/types.go
+++ entityDumper/types.go
@@ -1,11 +1,39 @@
 package entityDumper
 
+import (
+	"github.com/rs/zerolog/log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type ChannelDumperOptions struct {
 	ServerConfig string
 	ChannelLabels []string
 	ChannelWithChildrenLabels []string
 	OutputFolder string
+	outputFolderAbsPath string
 	MetadataOnly bool
+}
+
+func (opt *ChannelDumperOptions) getOutputFolderAbsPath() string {
+	if "" == opt.outputFolderAbsPath {
+		outputFolder := opt.OutputFolder
+		if filepath.IsAbs(outputFolder) {
+			outputFolder, _ = filepath.Abs(outputFolder)
+		} else {
+			homedir, err := os.UserHomeDir()
+			if err != nil {
+				log.Fatal().Msg("Couldn't determine the home directory")
+				panic(err)
+			}
+			if strings.HasPrefix(outputFolder, "~") {
+				outputFolder = strings.Replace(outputFolder, "~", homedir, -1)
+			}
+		}
+		opt.outputFolderAbsPath =outputFolder
+	}
+	return opt.outputFolderAbsPath
 }
 
 type channelsProcess struct {

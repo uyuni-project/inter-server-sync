5ce30dca9888bff4d99b3c026555f89fcf3793cf
Artem Shiliaev <artem.shiliaev@suse.com>
2022-01-31 17:47:38 +0100

    fallback to most columns index

--- schemareader/reader_test.go
+++ schemareader/reader_test.go
@@ -7,67 +7,84 @@ import (
 	"testing"
 )
 
+const (
+	TableName          = "TableName"
+	PKColumnName       = "PKColumnName"
+	UniqueIndexName01  = "UniqueIndexName01"
+	UniqueIndexName02  = "UniqueIndexName02"
+	IndexColumnName    = "IndexColumnName"
+	NonIndexColumnName = "NonIndexColumnName"
+)
+
 func TestProcessTable(t *testing.T) {
 
-	tableName := "tableName"
-	pKCol := "pKCol"
-	uniqueIndexName := "uniqueIndexName"
-	indexCol := "uniqueColumn"
-	nonIndexCol := "nonUniqueCol"
-	expectedIndexes := map[string]UniqueIndex{
-		uniqueIndexName: {
-			Name:    uniqueIndexName,
-			Columns: []string{indexCol},
-		},
-	}
-
+	// Arrange
 	repo := tests.CreateDataRepository()
+	UniqueIndexMostColumnsCase(repo)
+
+	// Act
+	table := processTable(repo.DB, TableName, true)
+
+	// Assert
+	indexesEqual := reflect.DeepEqual(table.MainUniqueIndexName, UniqueIndexName02)
+	if !indexesEqual {
+		t.Errorf("UniqueIndexes are not expected.")
+	}
+}
+
+func UniqueIndexMostColumnsCase(repo *tests.DataRepository) {
 	repo.ExpectWithRecords(
 		ReadColumnNames,
 		sqlmock.NewRows([]string{"column_name"}).
-			AddRow(pKCol).
-			AddRow(nonIndexCol).
-			AddRow(indexCol),
-		tableName,
+			AddRow(PKColumnName).
+			AddRow(NonIndexColumnName).
+			AddRow(IndexColumnName),
+		TableName,
 	)
 	repo.ExpectWithRecords(
 		ReadPkColumnNames,
 		sqlmock.NewRows([]string{"attname"}).
-			AddRow(pKCol),
-		tableName,
+			AddRow(PKColumnName),
+		TableName,
 	)
 	repo.ExpectWithRecords(
 		ReadPkSequence,
 		sqlmock.NewRows([]string{"sequence_name"}),
-		tableName,
+		TableName,
 	)
+
+	// Read indexes information
 	repo.ExpectWithRecords(
 		ReadUniqueIndexNames,
 		sqlmock.NewRows([]string{"indexrelid"}).
-			AddRow(uniqueIndexName),
-		tableName,
+			AddRow(UniqueIndexName01).
+			AddRow(UniqueIndexName02),
+		TableName,
 	)
 	repo.ExpectWithRecords(
 		ReadIndexColumns,
 		sqlmock.NewRows([]string{"indexrelid"}).
-			AddRow(indexCol),
-		uniqueIndexName,
+			// One column in the index
+			AddRow(PKColumnName),
+		UniqueIndexName01,
 	)
+	repo.ExpectWithRecords(
+		ReadIndexColumns,
+		sqlmock.NewRows([]string{"indexrelid"}).
+			// Two columns in the index
+			AddRow(PKColumnName).
+			AddRow(IndexColumnName),
+		UniqueIndexName02,
+	)
+
 	repo.ExpectWithRecords(
 		ReadReferenceConstraintNames,
 		sqlmock.NewRows([]string{"constraint_name"}),
-		tableName,
+		TableName,
 	)
 	repo.ExpectWithRecords(
 		ReadReferencedByConstraintNames,
 		sqlmock.NewRows([]string{"constraint_name"}),
-		tableName,
+		TableName,
 	)
-
-	table := processTable(repo.DB, tableName, true)
-
-	indexesEqual := reflect.DeepEqual(table.UniqueIndexes, expectedIndexes)
-	if !indexesEqual {
-		t.Errorf("UniqueIndexes are not expected.")
-	}
 }

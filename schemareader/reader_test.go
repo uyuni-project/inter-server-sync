839ab084be5c8f7b8ab5db66c97f55254d5f0370
Artem Shiliaev <artem.shiliaev@suse.com>
2022-01-31 17:58:48 +0100

    extend test

--- schemareader/reader_test.go
+++ schemareader/reader_test.go
@@ -8,11 +8,15 @@ import (
 )
 
 const (
-	TableName         = "TableName"
-	PKColumnName      = "PKColumnName"
+	TableName    = "TableName"
+	PKColumnName = "PKColumnName"
+
 	UniqueIndexName01 = "UniqueIndexName01"
 	UniqueIndexName02 = "UniqueIndexName02"
-	IndexColumnName   = "IndexColumnName"
+	UniqueIndexName03 = "UniqueIndexName03"
+
+	IndexColumnName01 = "IndexColumnName01"
+	IndexColumnName02 = "IndexColumnName02"
 )
 
 func TestProcessTable(t *testing.T) {
@@ -25,9 +29,9 @@ func TestProcessTable(t *testing.T) {
 	table := processTable(repo.DB, TableName, true)
 
 	// Assert
-	indexesEqual := reflect.DeepEqual(table.MainUniqueIndexName, UniqueIndexName02)
+	indexesEqual := reflect.DeepEqual(table.MainUniqueIndexName, UniqueIndexName03)
 	if !indexesEqual {
-		t.Errorf("UniqueIndexes do not match: expected %s, got %s", UniqueIndexName02, table.MainUniqueIndexName)
+		t.Errorf("UniqueIndexes do not match: expected %s, got %s", UniqueIndexName03, table.MainUniqueIndexName)
 	}
 }
 
@@ -37,14 +41,16 @@ func UniqueIndexMostColumnsCase(repo *tests.DataRepository) {
 	repo.ExpectWithRecords(ReadPkColumnNames, sqlmock.NewRows([]string{"attname"}), TableName)
 	repo.ExpectWithRecords(ReadPkSequence, sqlmock.NewRows([]string{"sequence_name"}), TableName)
 
-	// Read indexes information
+	// Read indexes information to get three indexes
 	repo.ExpectWithRecords(
 		ReadUniqueIndexNames,
 		sqlmock.NewRows([]string{"indexrelid"}).
 			AddRow(UniqueIndexName01).
-			AddRow(UniqueIndexName02),
+			AddRow(UniqueIndexName02).
+			AddRow(UniqueIndexName03),
 		TableName,
 	)
+	// Read columns for index UniqueIndexName01
 	repo.ExpectWithRecords(
 		ReadIndexColumns,
 		sqlmock.NewRows([]string{"indexrelid"}).
@@ -52,13 +58,24 @@ func UniqueIndexMostColumnsCase(repo *tests.DataRepository) {
 			AddRow(PKColumnName),
 		UniqueIndexName01,
 	)
+	// Read columns for index UniqueIndexName02
 	repo.ExpectWithRecords(
 		ReadIndexColumns,
 		sqlmock.NewRows([]string{"indexrelid"}).
 			// Two columns in the index
 			AddRow(PKColumnName).
-			AddRow(IndexColumnName),
+			AddRow(IndexColumnName01),
 		UniqueIndexName02,
+	)
+	// Read columns for index UniqueIndexName03
+	repo.ExpectWithRecords(
+		ReadIndexColumns,
+		sqlmock.NewRows([]string{"indexrelid"}).
+			// Three columns in the index
+			AddRow(PKColumnName).
+			AddRow(IndexColumnName01).
+			AddRow(IndexColumnName02),
+		UniqueIndexName03,
 	)
 
 	repo.ExpectWithRecords(ReadReferenceConstraintNames, sqlmock.NewRows([]string{"constraint_name"}), TableName)

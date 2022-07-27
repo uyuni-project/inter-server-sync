82e041224e8b165cc53fb2b604b2c6d15fcb8198
Ricardo Mateus <rmateus@suse.com>
2022-07-27 18:00:46 +0100

    improve log messages
    Signed-off-by: Ricardo Mateus <rmateus@suse.com>

--- dumper/packageDumper/packageDumper.go
+++ dumper/packageDumper/packageDumper.go
@@ -3,7 +3,8 @@ package packageDumper
 import (
 	"database/sql"
 	"fmt"
-	"log"
+	"github.com/rs/zerolog/log"
+	"time"
 
 	"github.com/uyuni-project/inter-server-sync/dumper"
 	"github.com/uyuni-project/inter-server-sync/schemareader"
@@ -17,23 +18,47 @@ func DumpPackageFiles(db *sql.DB, schemaMetadata map[string]schemareader.Table, 
 	table := schemaMetadata[packageKeysData.TableName]
 	pathIndex := table.ColumnIndexes["path"]
 
+	totalPackages := len(packageKeysData.Keys)
+	log.Debug().Msgf("Total package files to copy: %d", totalPackages)
+
+	exportedpackages := 0
+	processing := true
+
+	if log.Debug().Enabled() {
+		go func() {
+			count := 0
+			for {
+				if !processing {
+					break
+				}
+				time.Sleep(30 * time.Second)
+				log.Debug().Msgf("#count: %d -- #exportedPackageFiles: #%d of %d",
+					count, totalPackages, exportedpackages)
+				count++
+			}
+		}()
+	}
+
 	exportPoint := 0
 	batchSize := 500
+
 	for len(packageKeysData.Keys) > exportPoint {
 		upperLimit := exportPoint + batchSize
 		if upperLimit > len(packageKeysData.Keys) {
 			upperLimit = len(packageKeysData.Keys)
 		}
 		rows := dumper.GetRowsFromKeys(db, table, packageKeysData.Keys[exportPoint:upperLimit])
+		exportedpackages += len(rows)
 		for _, rowPackage := range rows {
 			path := rowPackage[pathIndex]
 			source := fmt.Sprintf("%s/%s", serverDataFolder, path.Value)
 			target := fmt.Sprintf("%s/%s", outputFolder, path.Value)
 			_, error := dumper.Copy(source, target)
 			if error != nil {
-				log.Panic("could not Copy File: ", error)
+				log.Panic().Err(error).Msg("could not Copy File")
 			}
 		}
 		exportPoint = upperLimit
 	}
+	processing = false
 }
